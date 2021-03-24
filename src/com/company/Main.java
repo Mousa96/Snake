be4c61353b11b1841cbb56390d9a0d177d3804b6
Mousa96 <muhammad.moussa.eg@gmail.com>
2021-03-25 01:42:55 +0200

    saving project

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+    }
+}
+
+/* Steps to write to the program:
+    1- Create a frame for the game
+    2- add a panel to the frame
+    3- inside of the panel start by defining the methods that should be executed
+    4- define the fields that you will need to execute your methods
+
+    methods:
+    1- Start the game
+    2- draw your spirites
+    3- add algorithm for snake movements
+    4- add logic to map key strokes to snake movement
+    5- detect if apple is eaten and update score
+    6- detect if collision happens and end game
+ */
